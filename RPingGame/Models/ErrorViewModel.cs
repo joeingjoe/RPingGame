8bb65211bb2afbf7c9d4165af9607ade0f0a36eb
joeingjoe <joeingjoe3@gmail.com>
2023-07-26 18:43:13 +0800

    加入專案檔案。

---
+++ RPingGame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPingGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
